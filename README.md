0174c4887c941c8ffd100c7429a7ca265ff0bc44
Lisandro Crespo <hey@lisandroct.com>
2020-01-28 09:20:32 -0300

    Add writer

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Hello, love
-
 Lichi loves Gigi. :heart:
+
+This public repository will be stored forever in the Github Arctic Code Vault. It's my nerd version of carving our names on a tree or putting a lock on a bridge.
+
+## Build
+Clone the repository, navigate to its root folder and run the command `./gradlew shadowJar` from a console.
+
+## Run
+Navigate to `build/libs` and run `java -jar hello-love-1.0-all.jar`.
+
+## Demo
---
+++ src/main/java/com/gandl/hellolove/Writer.java
@@ -0,0 +1,39 @@
+package com.gandl.hellolove;
+
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Writer {
+    public static void print(Terminal terminal, String string) {
+        try {
+            Thread.sleep(700);
+            terminal.clearScreen();
+            terminal.setCursorPosition(0, 0);
+            for(int i = 0, n = string.length(); i < n; i++) {
+                terminal.putCharacter(string.charAt(i));
+                terminal.flush();
+                Thread.sleep(300);
+            }
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void backspace(Terminal terminal, String string, int count) {
+        try {
+            Thread.sleep(700);
+            for(int i = 0; i < count; i++) {
+                terminal.clearScreen();
+                terminal.setCursorPosition(0, 0);
+                for(int j = 0, n = string.length() - i; j < n; j++) {
+                    terminal.putCharacter(string.charAt(j));
+                }
+                terminal.flush();
+                Thread.sleep(300);
+            }
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
